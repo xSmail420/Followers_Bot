33a57b6d503dd1e9c02848aee7322071582bd9ab
xSmail420 <tnknight97@gmail.com>
2023-12-19 02:29:52 +0100

    Final Instagram followers Bot

---
+++ Instagram.py
@@ -0,0 +1,150 @@
+import argparse
+import json
+import time
+import datetime
+from instagram.InstaBot import InstagramBot
+
+def load_accounts():
+        try:
+            with open("instagram/accounts.json", "r") as file:
+                accounts = json.load(file)
+        except FileNotFoundError:
+            accounts = []
+        return accounts
+
+def load_users():
+        try:
+            with open("instagram/users.json", "r") as file:
+                users = json.load(file)
+            return users
+        except FileNotFoundError:
+            return None
+     
+def save_accounts(accounts):
+    with open("instagram/accounts.json", "w") as file:
+        json.dump(load_accounts() + accounts, file)
+
+def save_followed(users):
+    with open("instagram/followed.json", "w") as file:
+        json.dump(users, file)
+
+def load_followed():
+    try:
+        with open("instagram/followed.json", "r") as file:
+            users = json.load(file)
+            # Assurez-vous que 'users' est une liste
+            if not isinstance(users, list):
+                users = []
+        return users
+    except FileNotFoundError:
+        return []
+
+def create_bot(account):
+    bot = InstagramBot()
+    bot.login(email=account["username"], password=account["password"])
+    return bot
+
+def main():
+    parser = argparse.ArgumentParser(description="Instagram Bot")
+
+    parser.add_argument("-a", "--accounts", action="store_true", help="Use multiple accounts")
+    parser.add_argument("-d", "--days", type=int, default=7, help="Days before unfollow users")
+    parser.add_argument("-del", "--delay", type=int, default=5, help="Delay in seconds between actions")
+
+    args = parser.parse_args()
+
+    if args.accounts:
+        accounts = load_accounts()
+
+        if len(accounts) == 0:
+            print("No accounts found. Please add accounts first.")
+            return
+
+        for i, account in enumerate(accounts):
+            print(f"Account {i+1}: {account['username']}")
+
+        account_choice = input("Select an account number: ")
+
+        try:
+            account_choice = int(account_choice)
+            if account_choice < 1 or account_choice > len(accounts):
+                print("Invalid account number.")
+                return
+        except ValueError:
+            print("Invalid input.")
+            return
+
+        account = accounts[account_choice - 1]
+        bot = create_bot(account)
+    else:
+        username = input("Enter your Instagram username: ")
+        password = input("Enter your Instagram password: ")
+
+        account = {
+            "username": username,
+            "password": password
+        }
+        save_accounts([account])
+        bot = create_bot(account)
+
+    
+    usernames = load_users()
+    followed = load_followed()
+    follow_actions = 0
+    unfollow_actions = 0
+    queue = []
+    
+    while True:
+
+        if follow_actions > 90 or unfollow_actions >90 :
+            # 190 actions per day
+            print("sleep until daily limit resets")
+            actions = 0
+            time.sleep(24 * 60 * 60)
+            continue
+
+        # Get the current date
+        current_date = datetime.datetime.now().date()
+        users_to_follow = []
+
+        # Iterate over the followed list and unfollow users with dates more than provided days ago
+        for entry in followed:
+
+            if unfollow_actions > 190 :
+                print("daily unfollow actions limit reached...")
+                break
+
+            entry_date = datetime.datetime.strptime(entry["date"], "%m/%d/%Y").date()
+            days_difference = (current_date - entry_date).days
+
+
+            if days_difference >= args.days:
+                # Unfollow the user
+                # bot.unfollow(user = entry["username"], delay = args.delay)
+                print(f'unfollow action : {entry["username"]}')
+                actions += 1
+            else :
+                queue.append(entry)
+                
+
+        for username in usernames:
+            usernames.remove(username)
+            followers = bot.getfollowers(username=username, delay=args.delay)
+            users_to_follow.extend([user for user in followers if user not in [item['username'] for item in followed]])
+
+        for user in users_to_follow:
+            if follow_actions > 90 :
+                print("daily follow actions limit reached...")
+                break
+            # bot.follow(user = user,delay = args.delay)
+            print(f'follow action : {user}')
+            actions += 1
+            queue.append({"username": user, "date": datetime.datetime.now().strftime("%m/%d/%Y")})
+            
+        print(queue)
+        save_followed(queue)
+
+    bot.quit()
+
+if __name__ == "__main__":
+    main()
---
+++ instagram/InstaBot.py
@@ -0,0 +1,109 @@
+import time
+import json
+import random
+from datetime import datetime as dt
+#from selenium import webdriver
+import undetected_chromedriver as uc
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.action_chains import ActionChains
+
+class InstagramBot:
+    def __init__(self):
+        chrome_options = Options()
+        chrome_options.add_argument("--window-size=930,820")
+        chrome_options.add_argument("--headless")  # Run Chrome in headless mode
+        # chrome_options.add_argument("--start-maximized")  # Maximize the Chrome window
+        # Use webdriver_manager to automatically download and manage the ChromeDriver
+        # add undetected_chromedriver here 
+        self.driver = uc.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
+
+    def login(self, email, password):
+        # Open Instagram
+        self.driver.get("https://www.instagram.com/")
+        # Wait for the login elements to become available
+        wait = WebDriverWait(self.driver, 10)
+        email_field = wait.until(EC.presence_of_element_located((By.NAME, "username")))
+        
+        password_field = wait.until(EC.presence_of_element_located((By.NAME, "password")))
+        
+        # Find the login elements and enter email and password
+        email_field.send_keys(email)
+        time.sleep(3)
+        password_field.send_keys(password)
+        time.sleep(1)
+        # Submit the login form
+        password_field.send_keys(Keys.RETURN)
+        print(f"Logged in as : {email}")
+        # Wait for the login process to complete (you may need to adjust the delay based on your internet speed)
+        time.sleep(10)  # Wait for 5 seconds (adjust as needed)
+   
+    def getfollowers(self, username, delay):
+        names = []
+        try:
+            link = self.Scrape_post_url(username=username,delay_time=delay)
+            self.driver.get(link.replace("reel", "p")+"liked_by/")
+            time.sleep(delay)
+            # Get likes list
+            wait = WebDriverWait(self.driver, 10)
+            wait.until(EC.presence_of_element_located((By.XPATH, '//section/main/div[1]/div/div')))
+
+            like_list = self.driver.find_elements(By.XPATH,'//section/main/div[1]/div/div//div[2]//a')
+            for user_url in like_list:
+                names.append(user_url.get_attribute("href").split("/")[-2]) 
+            time.sleep(delay)
+        except:
+            return []
+        return list(set(names))
+    
+    def follow(self, user, delay):
+        self.driver.get(f"https://www.instagram.com/{user}")
+        wait = WebDriverWait(self.driver, 10)
+        follow_btn = wait.until(EC.presence_of_element_located((By.XPATH, '//section/div[1]/div[2]/div/div[1]/button')))
+        
+        if follow_btn.text.lower() == "follow":
+            follow_btn.click()
+            time.sleep(10)
+
+    def unfollow(self, user, delay):
+        self.driver.get(f"https://www.instagram.com/{user}")
+        wait = WebDriverWait(self.driver, 10)
+        following_btn = wait.until(EC.presence_of_element_located((By.XPATH, '//section/div[1]/div[2]/div/div[1]/button')))
+        
+        if following_btn.text.lower() == "following":
+            following_btn.click()
+            time.sleep(3)
+            wait = WebDriverWait(self.driver, 10)
+            unfollow_btn = wait.until(EC.presence_of_element_located((By.XPATH, '//div[2]/div/div/div/div[8]')))
+            if unfollow_btn.text.lower() == "unfollow":
+                unfollow_btn.click()
+                time.sleep(3)
+    
+    def Scrape_post_url(self, username, delay_time): 
+        latest_post = ""
+        try:
+                # Open Instagram and navigate to the user stories page
+                self.driver.get(f"https://www.instagram.com/{username}/")
+
+                # Wait for the stories to load
+                wait = WebDriverWait(self.driver, 10)
+                wait.until(EC.presence_of_element_located((By.XPATH, '//div/div[3]/article/div[1]/div')))
+
+                most_recent = self.driver.find_element(By.XPATH, '//div/div[3]/article/div[1]/div')
+            
+                # Scrape the most recent posts from the profile
+                posts = most_recent.find_elements(By.TAG_NAME, "a")
+                # Retrieve the href attribute value
+                latest_post = posts[0].get_attribute("href")
+        except :
+            pass
+
+        return latest_post
+
+    def quit(self):
+        self.driver.quit()
